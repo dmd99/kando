71beea91eb316f417e1a1f9ece3c3a10ddc0a050
dmd99 <nmaboury@gmail.com>
2021-02-14 01:27:07 +0100

    last last last

--- src/pages/About.js
+++ src/pages/About.js
@@ -8,10 +8,10 @@ const About = () => {
         <Layout title="A propos" description="This is the About page" >
             <div className="text-center mt-5">
                 <h1>About</h1>
-                <p>This project was built for practice purposes using <strong>Context API</strong> by React.</p>
+                <p>Bayileen sope parégouma et en plus <strong>J'ai pas d'inspiration en tout cas </strong> Ciao.</p>
 
-                <a className="btn btn-primary" href="https://github.com/AlexSegen/react-shopping-cart">
-    /> <span className="ml-2 mr-4">a PRORPOS</span></a>
+                <a className="btn btn-primary" href="https://pornhub.com">
+                    <span className="ml-2 mr-4">Ne cliquez pas sur ce boutton</span></a>
             </div>
             <Details/>
         </Layout>
